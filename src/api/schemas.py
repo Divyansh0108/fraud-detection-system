6f987ed6e061abc5af4dffe8952fa2cda4a700d4
Divyansh Pandey <113619486+Divyansh0108@users.noreply.github.com>
2026-01-14 20:34:05 +0530

    Developed schema

---
+++ src/api/schemas.py
@@ -0,0 +1,15 @@
+from pydantic import BaseModel, Field
+from typing import List
+
+class Transaction(BaseModel):
+    Time: float = Field(..., description="Time in seconds since first transaction")
+    Amount: float = Field(..., description="Transaction amount")
+    V_features: List[float] = Field(
+        ..., min_length=28, max_length=28, description="PCA components V1-V28"
+    )
+
+class PredictionResponse(BaseModel):
+    transaction_id: str
+    is_fraud: bool
+    probability: bool
+    latency_ms: float
